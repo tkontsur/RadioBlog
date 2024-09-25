827c11ec0271a744215643aba01fdc5ee4926e5f
Taras Kontsur <kontsur@gmail.com>
2024-09-25 22:11:42 +0300

    Fixed Spring test configuration

--- BE/RadioBlog/src/test/java/com/radioblog/RadioBlogApplicationTests.java
+++ BE/RadioBlog/src/test/java/com/radioblog/RadioBlogApplicationTests.java
@@ -2,8 +2,10 @@ package com.radioblog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RadioBlogApplicationTests {
 
     @Test
--- BE/RadioBlog/src/test/java/com/radioblog/service/UserServiceTest.java
+++ BE/RadioBlog/src/test/java/com/radioblog/service/UserServiceTest.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UserServiceTest {
     @Autowired
     private UserService userService;
